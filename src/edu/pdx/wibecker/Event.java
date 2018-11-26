edc7fdf6b8996361bc9a8336622d62b3d17b86b0
will <willvfs@gmail.com>
2018-11-25 20:55:37 -0800

    Initial commit

---
+++ src/edu/pdx/wibecker/Event.java
@@ -0,0 +1,17 @@
+package edu.pdx.wibecker;
+
+public class Event
+{
+    protected String left_path;
+    protected String right_path;
+    protected String name;
+    protected String description;
+
+    public Event()
+    {
+        left_path = null;
+        right_path = null;
+        name = null;
+        description = null;
+    }
+}
---
+++ src/edu/pdx/wibecker/Main.java
@@ -0,0 +1,8 @@
+package edu.pdx.wibecker;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
